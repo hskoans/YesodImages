633db70b509c93b0da520d8f64ffb66d6819cf20
Calvin Cheng <linchuan.cheng@gmail.com>
2015-06-30 23:32:18 +0800

    Update shell.nix and YesodImages.cabal to conform with install on Darwin

--- shell.nix
+++ shell.nix
@@ -1,36 +1,44 @@
-with (import <nixpkgs> {}).pkgs;
-let pkg = haskellngPackages.callPackage
-            ({ mkDerivation, aeson, base, bytestring, classy-prelude
-             , classy-prelude-conduit, classy-prelude-yesod, conduit, containers
-             , data-default, directory, fast-logger, file-embed, hjsmin, hspec
-             , http-conduit, monad-control, monad-logger, persistent
-             , persistent-postgresql, persistent-template, resourcet, safe
-             , shakespeare, stdenv, template-haskell, text, time, transformers
-             , unordered-containers, vector, wai-extra, wai-logger, warp, yaml
-             , yesod, yesod-auth, yesod-core, yesod-form, yesod-static
-             , yesod-test
-             }:
-             mkDerivation {
-               pname = "YesodImages";
-               version = "0.0.0";
-               src = ./.;
-               isLibrary = true;
-               isExecutable = true;
-               buildDepends = [
-                 aeson base bytestring classy-prelude classy-prelude-conduit
-                 classy-prelude-yesod conduit containers data-default directory
-                 fast-logger file-embed hjsmin http-conduit monad-control
-                 monad-logger persistent persistent-postgresql persistent-template
-                 safe shakespeare template-haskell text time unordered-containers
-                 vector wai-extra wai-logger warp yaml yesod yesod-auth yesod-core
-                 yesod-form yesod-static
-               ];
-               testDepends = [
-                 base classy-prelude classy-prelude-yesod hspec monad-logger
-                 persistent persistent-postgresql resourcet shakespeare transformers
-                 yesod yesod-core yesod-test
-               ];
-               license = stdenv.lib.licenses.unfree;
-             }) {};
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "ghc7101" }:
+
+let
+
+  inherit (nixpkgs) pkgs;
+
+  f = { mkDerivation, aeson, base, bytestring, classy-prelude
+      , classy-prelude-conduit, classy-prelude-yesod, conduit, containers
+      , data-default, directory, fast-logger, file-embed, hjsmin, hspec
+      , http-conduit, monad-control, monad-logger, persistent
+      , persistent-postgresql, persistent-template, resourcet, safe
+      , shakespeare, stdenv, template-haskell, text, time, transformers
+      , unordered-containers, vector, wai-extra, wai-logger, warp, yaml
+      , yesod, yesod-auth, yesod-core, yesod-form, yesod-static
+      , yesod-test
+      }:
+      mkDerivation {
+        pname = "YesodImages";
+        version = "0.0.0";
+        src = ./.;
+        isLibrary = true;
+        isExecutable = true;
+        buildDepends = [
+          aeson base bytestring classy-prelude classy-prelude-conduit
+          classy-prelude-yesod conduit containers data-default directory
+          fast-logger file-embed hjsmin http-conduit monad-control
+          monad-logger persistent persistent-postgresql persistent-template
+          safe shakespeare template-haskell text time unordered-containers
+          vector wai-extra wai-logger warp yaml yesod yesod-auth yesod-core
+          yesod-form yesod-static
+        ];
+        testDepends = [
+          base classy-prelude classy-prelude-yesod hspec monad-logger
+          persistent persistent-postgresql resourcet shakespeare transformers
+          yesod yesod-core yesod-test
+        ];
+        license = stdenv.lib.licenses.unfree;
+      };
+
+  drv = pkgs.haskell.packages.${compiler}.callPackage f {};
+
 in
-  pkg.env
+
+  if pkgs.lib.inNixShell then drv.env else drv
